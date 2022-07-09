a4584b2bc332fc51bc4e4937c7d65b4d4859687f
özcan çetin <cetin445@gmail.com>
2022-07-09 22:00:26 +0300

    router completed

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Details from "../pages/details/Details"
+import Home from "../pages/home/Home"
+import Login from "../pages/login/Login";
+import About from "../pages/about/About";
+import Navbar from "../components/navbar/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='details' element={<Details/>}/>
+        <Route path='login' element={<Login/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
